0cb51adb55b241cbb001a59dd12d2a7082e72fa7
rrih <origabird0911@gmail.com>
2023-09-08 03:37:25 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,158 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React, { useEffect, useRef, useState } from "react";
+
+const Home: React.FC = () => {
+  const [image, setImage] = useState<HTMLImageElement | null>(null);
+  const [emoji, setEmoji] = useState<string>("😁");
+  const [emojiPosition, setEmojiPosition] = useState({ x: 50, y: 50 });
+  const [emojiSize, setEmojiSize] = useState<number>(50);
+  const [maxEmojiSize, setMaxEmojiSize] = useState<number>(100);
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const emojiOptions = ["😁", "🥺", "😤", "😭", "😢", "🥲", "😡"];
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      const img = new Image();
+      img.onload = () => {
+        setImage(img);
+        setMaxEmojiSize(img.height);
+        draw();
+      };
+      img.src = event.target?.result as string;
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const draw = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // 画像が存在する場合のみ描画
+    if (image) {
+      // 画像のアスペクト比を維持しながら、キャンバスにフィットするようにリサイズ
+      const aspectRatio = image.width / image.height;
+      const canvasWidth = Math.min(window.innerWidth, 600); // window.innerWidthまたは600pxの小さい方
+      const canvasHeight = canvasWidth / aspectRatio;
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    }
+
+    ctx.font = `${emojiSize}px Arial`;
+    ctx.fillText(emoji, emojiPosition.x, emojiPosition.y);
+
+    // ダウンロード用URLを更新
+    setDownloadUrl(canvas.toDataURL());
+  };
+
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "16px",
+  };
+
+  const canvasStyle = {
+    maxWidth: "100%",
+  };
+
+  useEffect(() => {
+    draw();
+  }, [emoji, emojiSize, emojiPosition]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div style={containerStyle}>
+      <div
+        style={{ marginBottom: "16px", display: "flex", alignItems: "center" }}
+      >
+        <button
+          onClick={handleButtonClick}
+          style={{
+            marginRight: "12px",
+            padding: "8px 16px",
+            background: "#007bff",
+            color: "#fff",
+            border: "none",
+            borderRadius: "4px",
+          }}
+        >
+          Upload Image
+        </button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleImageChange}
+          style={{ display: "none" }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <select
+        value={emoji}
+        onChange={(e) => {
+          setEmoji(e.target.value);
+          draw();
+        }}
+        style={{ marginBottom: "16px" }}
+      >
+        {emojiOptions.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <canvas
+        ref={canvasRef}
+        width={600}
+        height={400}
+        onClick={(e) => {
+          const rect = e.currentTarget.getBoundingClientRect();
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+          setEmojiPosition({ x, y });
+        }}
+        style={canvasStyle}
+      ></canvas>
+      <br />
+      <label>
+        Emoji size:
+        <input
+          type="range"
+          min="10"
+          max={maxEmojiSize}
+          value={emojiSize}
+          onChange={(e) => {
+            setEmojiSize(Number(e.target.value));
+          }}
+        />
+      </label>
+      <br />
+      {downloadUrl && (
+        <a href={downloadUrl} download="emoji_image.png">
+          Download Image
         </a>
+      )}
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
